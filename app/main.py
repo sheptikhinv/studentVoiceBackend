2fb4630b63a743a3a2a0ddb14d7da0ff09a74438
shept <sheptikhinv@gmail.com>
2024-10-29 13:51:52 +0500

    specialized uvicorn args

--- app/main.py
+++ app/main.py
@@ -10,4 +10,4 @@ def start_server() -> None:
     Initialize the database and start the uvicorn server
     """
     asyncio.run(init_db())
-    uvicorn.run("app.routers.main:app")
+    uvicorn.run("app.routers.main:app", host="127.0.0.1", port=5001, root_path="/api")
