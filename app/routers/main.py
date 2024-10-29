3bc6bfa685f00a39ca2a8602c2ef8d3d48663a9b
shept <sheptikhinv@gmail.com>
2024-10-29 14:04:00 +0500

    fixed swagger?

--- app/routers/main.py
+++ app/routers/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from .auth import router as auth_router
 from .admin import admin_router
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 app.include_router(admin_router)
 app.include_router(auth_router)
